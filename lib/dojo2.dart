0a363654a7c2ec55685350efd8f84051ccf62483
thaysg <thay.a.garcia@gmail.com>
2022-01-11 19:02:25 -0300

    testando com 40

--- lib/dojo2.dart
+++ lib/dojo2.dart
@@ -5,7 +5,48 @@ void main() {
 //Quarta rodada vivos 1, 4
 //Quinta rodada vivos 4
 
-  List<int> n = [1, 2, 3, 4, 5]; //Números de vivos
+  List<int> n = [
+    1,
+    2,
+    3,
+    4,
+    5,
+    6,
+    7,
+    8,
+    9,
+    10,
+    11,
+    12,
+    13,
+    14,
+    15,
+    16,
+    17,
+    18,
+    19,
+    20,
+    21,
+    22,
+    23,
+    24,
+    25,
+    26,
+    27,
+    28,
+    29,
+    30,
+    31,
+    32,
+    33,
+    34,
+    35,
+    36,
+    37,
+    38,
+    39,
+    40,
+  ]; //Números de vivos
   int i = 1; //contador de vivos
   int k = 3; //contador de mortos
   print('Lista Inicial: $n');
